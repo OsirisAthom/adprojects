b661ca1da60ad253ad40beebee111abb0d008789
OsirisAthom <129094393+OsirisAthom@users.noreply.github.com>
2025-03-11 21:51:12 -0600

    Update cortarfilas.py

--- cortarfilas.py
+++ cortarfilas.py
@@ -7,17 +7,18 @@ Movido 05/09/24
 import pandas as pd
 import numpy as np
 # Elementos requeridos
-fibra = 'F5' # Fibra a selección
+fibra = 'Fi' # Fibra a selección
 rutad = '$HOME/Documents/datos_csv/' # Ruta de donde se tomarán los datos
-vol = [0,1,2,3,4,5,6,7,8,9,10]
-min = [1,2,3,4,5,6,7,8,9,10]
+vol = [0,1,2,3,4,5,6,7,8,9,10]       # Valores de volumen
+min = [1,2,3,4,5,6,7,8,9,10]         # Valores de minutos
 # lista de filas con ceros
-lceros = np.arange(751,1001,1,dtype=int) # Alterar valores de  ser necesario
+lceros = np.arange(valorinicial,valor final,1,dtype=int) # Alterar valores de  ser necesario
 # Función para cortar a los de una sola fibra
 def cortar(c,t):
     # Valor de concentracion
+    # Cambiarlos a string
     if c == 0:
-        con = '0'
+        con = '0' 
     elif c == 10:
         con = '1'
     else:
